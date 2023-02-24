95dc281bbd84b1e9f853238096ae42f8acb7e267
Troupin Charles <ctroupin@uliege.be>
2023-02-24 11:05:42 +0100

    parameters config + colors + URLs

--- analysis/config.jl
+++ analysis/config.jl
@@ -1,19 +1,27 @@
-# This is a TOML document
-
+# 
 title = "Calanus interpolation parameters"
+
+# Domain and resolution
 domain = [-20.5, 11.75, 41.25, 67.0]
 dlon = 0.25
 dlat = 0.25
 
-[directories]
+# Directories
 datadir = "../data/"
 figdir = "../figures/"
 datafigdir = "../figures/observations/"
 resfigdir = "../figures/L025/"
+wodfigdir = "../figures/"
 resdir = "../results/L025/"
 resdirm = "../results/month/L025"
 
-[files]
+# Files 
 datafile = "MBA_CPRdata_Emodnet_21Jan22.csv"
 datafileURL = "https://dox.ulg.ac.be/index.php/s/hjWKf1F3C1Pzz1r/download"
 bathyfile = "gebco_30sec_16.nc"
+bathyfileURL = "https://dox.ulg.ac.be/index.php/s/U0pqyXhcQrXjEUX/download"
+
+# Colors 
+# (to keep homogeneity accross the figures)
+mycolor = "#6667AB"
+mycolor2 = "#456A30" # Green treetop
